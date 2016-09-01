aeb2557b2ac7fcc8879e3eaa8fed81305d1101be
Jefferson Oliveira <jefferson351@gmail.com>
2016-09-01 12:20:09 -0300

    creating views

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,11 +2,11 @@ class Student < ActiveRecord::Base
   validates :name, :date_birth, :rg, :cpf, :date_emission, :organ_emitter,
             :nationality, :civil_status, :father_name, :mother_name, :email,
             :cnh, :profession, :scholarity, :vessel, :vessel_experience,
-            :payment_type, :document_photo, :proof_of_address, presence: true
+            :payment_type, presence: true
   has_many :phones
   has_one :address
   has_many :courses
   has_one :availability
   has_one :refferer
-  enum payment_type: [:credit_card, :bank_slip]
+  enum payment_type: { credit_card: 0, bank_slip: 1 }
 end
