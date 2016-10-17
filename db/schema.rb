b8ff52592b6fa498b94cf2670f60e53381f3286d
jefferson <cardbeatt@gmail.com>
2016-10-17 15:46:14 -0200

    change course column name value to price

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161005142035) do
+ActiveRecord::Schema.define(version: 20161017174351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20161005142035) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
-    t.decimal  "value",      precision: 8, scale: 2
+    t.decimal  "price",      precision: 8, scale: 2
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
   end
